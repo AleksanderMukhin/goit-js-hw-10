e4c21814b39d9beffb6cfb14bca89d88d1ba5da2
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-29 20:30:03 +0200

    task3 fix

--- src/index.js
+++ src/index.js
@@ -16,26 +16,38 @@ function onSearch(e) {
   e.preventDefault();
 
   const searchQuery = search.value;
-  console.log("🚀  searchQuery", searchQuery)
+  
+  if (!searchQuery) {
+    clearMarkup();
+    return;
+  }
 
   API.fetchCountries(searchQuery)
     .then(renderCountrys)
     .catch(onFetchError)
-  // .finally(() => reset());
-  
 }
 
 function onFetchError(error) {
-return Notiflix.Notify.failure(`❌ Oops, there is no country with that name`);
+  countryCard.innerHTML = "";
+  countryList.innerHTML = "";
+  Notiflix.Notify.failure(`❌ Oops, there is no country with that name`);
 };
 
+function clearMarkup() {
+  countryCard.innerHTML = "";
+  countryList.innerHTML = "";
+}
 
 function renderCountrys(country) {
   const quantity = country.length;
   
   if (quantity > 10) {
-    return Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
-  } else if(quantity <= 1) {const markup = country
+    Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
+    countryCard.innerHTML = "";
+    countryList.innerHTML = "";    
+  }
+  else if (quantity <= 1) {
+    const markup = country
     .map(({ capital, population, languages, flags, name }) => {
       return `<svg width="24" height="24" viewBox="0 0 24 24">
   <image href="${flags.svg}" width="24" height="24"></image></svg>
@@ -47,7 +59,8 @@ function renderCountrys(country) {
     .join("");
     countryCard.innerHTML = markup;
     countryList.innerHTML = "";
-  } else {
+  }
+  else {
     const mark = country
     .map(({ flags, name }) => {
       return `<p><svg width="24" height="24" viewBox="0 0 24 24">
@@ -59,40 +72,3 @@ function renderCountrys(country) {
     countryCard.innerHTML = "";
   }
 }
-  
-
-
-
-// const fetchPostsBtn = document.querySelector(".btn");
-// const userList = document.querySelector(".posts");
-
-// fetchPostsBtn.addEventListener("click", () => {
-//   fetchPosts()
-//     .then((posts) => renderPosts(posts))
-//     .catch((error) => console.log(error));
-// });
-
-// function fetchPosts() {
-//   // Change the number of items in the group here  
-//   return fetch("https://jsonplaceholder.typicode.com/posts?_limit=5").then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// }
-
-// function renderPosts(posts) {
-//   const markup = posts
-//     .map(({ id, title, body, userId }) => {
-//       return `<li>
-//           <h2 class="post-title">${title.slice(0, 30)}</h2>
-//           <p><b>Post id</b>: ${id}</p>
-//           <p><b>Author id</b>: ${userId}</p>
-//           <p class="post-body">${body}</p>
-//         </li>`;
-//     })
-//     .join("");
-//   userList.innerHTML = markup;
